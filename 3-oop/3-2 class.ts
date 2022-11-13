09778026a628cac4fbfcaf197fe326a71431cd5b
kks91 <kokyusik91@naver.com>
2022-11-13 19:10:55 +0900

    🚧 Class 연습

--- 3-oop/3-2 class.ts
+++ 3-oop/3-2 class.ts
@@ -1,39 +1,42 @@
 {
-
   type CoffeeCup = {
-  shots: number;
-  hasMilk: boolean;
-  }
-  
+    shots: number;
+    hasMilk: boolean;
+  };
+
   /**
    * 클래스를 만든 다는 것은 서로 관련 있는 데이터나 함수를 묶어 놓는 기능을 한다.
    */
   class CoffeeMaker {
     // 클래스의 data들은 위에 설정해 놓을 수 있다.
-    currentCoffeeBean: number = 0
-    static BEANS_GRAM_PER_SHOT: number = 7
-    constructor(coffeeBean : number) {
-      this.currentCoffeeBean = coffeeBean * CoffeeMaker.BEANS_GRAM_PER_SHOT
+    currentCoffeeBeanGram: number = 0;
+    static BEANS_GRAM_PER_SHOT: number = 7;
+    constructor(coffeeBean: number) {
+      this.currentCoffeeBeanGram = coffeeBean * CoffeeMaker.BEANS_GRAM_PER_SHOT;
     }
 
-    makeCoffee(shots: number): CoffeeCup  {
-      if (this.currentCoffeeBean < CoffeeMaker.BEANS_GRAM_PER_SHOT * shots) {
-        throw new Error('There is not enough bean to make coffee')
+    makeCoffee(shots: number): CoffeeCup {
+      if (
+        this.currentCoffeeBeanGram <
+        CoffeeMaker.BEANS_GRAM_PER_SHOT * shots
+      ) {
+        throw new Error('There is not enough bean to make coffee');
       }
-      
-      this.currentCoffeeBean -= CoffeeMaker.BEANS_GRAM_PER_SHOT * shots
+
+      this.currentCoffeeBeanGram -= CoffeeMaker.BEANS_GRAM_PER_SHOT * shots;
       return {
         shots,
-        hasMilk : true
-      }
+        hasMilk: true,
+      };
     }
-
   }
   // 처음에 커피 머신을 만들때, 충전할 커피량을 설정을 한다.
-  const maker = new CoffeeMaker(3)
-  console.log('maker',maker)
-  const maker2 = new CoffeeMaker(3)
-  console.log('maker2',maker2)
+  const maker = new CoffeeMaker(9);
+  console.log('maker', maker);
+  maker.makeCoffee(2);
+  console.log('maker', maker);
+  maker.makeCoffee(2);
+  console.log('maker', maker);
+
   // console.log(coffee.makeCoffee(2))
 }
-
---
+++ 3-oop/3-2-practice.ts
@@ -0,0 +1,55 @@
+{
+  type CoffeCup = {
+    shots: number;
+    hasMilk: false;
+  };
+
+  class CoffeeMaker {
+    private currentCoffeeBeanGrams: number;
+    // 이 기계가 1 Shot 당 사용하는 커피 그램
+    private static COFFEBEAN_PER_1SHOT: number = 7;
+
+    constructor(coffeeBean: number) {
+      // 커피 메이커 초기화 할때 커피 콩을 넣어줌.
+      this.currentCoffeeBeanGrams =
+        coffeeBean * CoffeeMaker.COFFEBEAN_PER_1SHOT;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+    // 커피기계를 만들어놓고 커피 콩을 추가하는 함수!!!
+    fillCoffeeBeans(beans: number) {
+      // 메서드 안에서 에러 핸들링을 함으로써 조금 더 정교하게 할 수 있다.
+      if (beans < 0) {
+        throw new Error('CoffeeBeans should be greater than 0');
+      }
+      this.currentCoffeeBeanGrams += beans * CoffeeMaker.COFFEBEAN_PER_1SHOT;
+    }
+    // 커피를 만드는 메서드
+    makeCoffee(shots: number): CoffeCup {
+      // 현재 커피 머신에 들어있는 커피 그램 보다 만들 커피가 더 많으면 에러
+      if (
+        this.currentCoffeeBeanGrams <
+        shots * CoffeeMaker.COFFEBEAN_PER_1SHOT
+      ) {
+        throw new Error('There is not enough CoffeeBean!');
+      }
+      this.currentCoffeeBeanGrams -= shots * CoffeeMaker.COFFEBEAN_PER_1SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+  // 일반적으로 인스턴스화 시키는 방법
+  const maker = new CoffeeMaker(5);
+  console.log('maker', maker);
+  maker.fillCoffeeBeans(1);
+  console.log('maker', maker);
+  // 내부에 선언된 메서드로 인스턴스화 시키는 법
+  const coffeeMachine = CoffeeMaker.makeMachine(17);
+  console.log('coffeeMachine', coffeeMachine);
+  coffeeMachine.fillCoffeeBeans(1);
+  console.log('coffeeMachine', coffeeMachine);
+}
