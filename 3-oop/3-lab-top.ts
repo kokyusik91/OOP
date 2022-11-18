f9874ea2494ef095c793f34f83345feb3f9b44c5
kks91 <kokyusik91@naver.com>
2022-11-18 14:48:24 +0900

    🚧  랩탑 내부기능들 추가하는 중....

--- 3-oop/3-lab-top.ts
+++ 3-oop/3-lab-top.ts
@@ -1,12 +1,17 @@
 {
   interface Computer {
-    powerOn(): void;
+    /**
+     *
+     * @param power 파워를 충전해줘야 컴퓨터가 켜집니다!
+     */
+    powerOn(power: number): void;
   }
 
   class LapTop implements Computer {
     private static powerStatus: boolean = false;
+    private static MINIMUM_POWER_NEEDED: number = 70;
+    private static currentPower: number = 0;
     constructor() {}
-    // 전원 키는데 필요한 전력
     static makeMachine(): LapTop {
       return new LapTop();
     }
@@ -27,6 +32,21 @@
       });
     }
 
+    // 전원 키는데 필요한 전력
+    // 충분한 파워 공급
+    private static chargePower(power: number) {
+      if (power < LapTop.MINIMUM_POWER_NEEDED)
+        throw new Error('we need more power charge');
+      else {
+        console.log(`${power}충전됨`);
+        console.log('@@@@ 파워 준비 완료! @@@@@');
+        LapTop.currentPower = power;
+        return true;
+      }
+    }
+    // 전원이 켜지자마자 실행되는 함수!
+    private static autoOff(currentPower: number) {}
+
     private static async booting() {
       if (LapTop.powerStatus === true) {
         const response = await LapTop.bootingPromise();
@@ -34,8 +54,8 @@
       } else throw new Error('먼저 전원을 켜주세요!');
     }
     // 전원 키는 메서드
-    async powerOn() {
-      if (LapTop.powerStatus === false) {
+    async powerOn(power: number) {
+      if (LapTop.powerStatus === false && LapTop.chargePower(power)) {
         LapTop.powerStatus = true;
         console.log('전원이 켜졌습니다!');
         await LapTop.booting();
@@ -56,8 +76,14 @@
   async function computer() {
     const myLabTop = new LapTop();
     console.log(myLabTop);
-    await myLabTop.powerOn();
+    await myLabTop.powerOn(71);
   }
 
   computer();
 }
+
+// 1. 전원 일정이상 충전! -> private (일정 충전량 이하면 에러)
+// 2. 파워 ON -> public
+//   2-1. 부팅중... -> private
+//   2-2. 윈도우 화면 켜짐 -> private
+// 3. 파워 OFF -> public
