80a46d7d84d8db21a0f40f131f90375107f4a73f
kks91 <kokyusik91@naver.com>
2022-11-20 23:39:45 +0900

    ✨ 함수에 제네릭 적용

--- 5-generic/5-1-function.ts
+++ 5-generic/5-1-function.ts
@@ -1,9 +1,21 @@
 {
-  function checkNotNull(arg: number | null): number{
+  function checkNotNullNumber(arg: number | null): number{
+    // null이면 에러 반환
     if (arg == null) {
       throw new Error('not valid number')
     }
 
     return arg;
   }
+
+  function checkNotNull<T>(arg: T | null): T {
+      if (arg == null) {
+      throw new Error('not valid number')
+    }
+
+    return arg;
+  }
+  // 코딩하는 시점에 checkNotNull 함수는 숫자타입으로 지정된다.
+  const number = checkNotNull(123);
+  const string : string = checkNotNull('hello');
 }
