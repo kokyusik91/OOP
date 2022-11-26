625e1455bb896f146724956474bdea161dd70e7e
kks91 <kokyusik91@naver.com>
2022-11-26 09:46:19 +0900

    🚧  제네릭 함수 복습하기

--- 5-generic/5-1-function.ts
+++ 5-generic/5-1-function.ts
@@ -1,21 +1,39 @@
 {
-  function checkNotNullNumber(arg: number | null): number{
+  function checkNotNullNumber(arg: number | null): number {
     // null이면 에러 반환
     if (arg == null) {
-      throw new Error('not valid number')
+      throw new Error('not valid number');
     }
 
     return arg;
   }
 
+  /**
+   *
+   * @param arg : generic
+   * @returns : generic
+   * 어떤 값을 인자로 받아서, null이 아니면 받은 타입 그대로 return 한다.
+   */
   function checkNotNull<T>(arg: T | null): T {
-      if (arg == null) {
-      throw new Error('not valid number')
+    if (arg == null) {
+      throw new Error('not valid number');
     }
 
     return arg;
   }
+
+  const checkNotNull2 = <T>(arg: T | null) => {
+    if (arg == null) {
+      throw new Error('not valid number');
+    }
+
+    return arg;
+  };
+
+  const number2 = checkNotNull2(123);
+  const hola: string = checkNotNull2('kyuskko');
+
   // 코딩하는 시점에 checkNotNull 함수는 숫자타입으로 지정된다.
   const number = checkNotNull(123);
-  const string : string = checkNotNull('hello');
+  const string: string = checkNotNull('hello');
 }
